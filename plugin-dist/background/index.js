783a8d30726ab89880573e642a9fd39fb8eb48ed
宁振宇 <ningzhenyu@youzan.com>
2022-01-04 21:01:29 +0800

    chrome 插件: content_scripts

--- plugin-dist/background/index.js
+++ plugin-dist/background/index.js
@@ -1,4 +1,5 @@
 chrome.runtime.onInstalled.addListener(function () {
+  console.log('chrome background run');
   // chrome 的 本地存储
   chrome.storage.sync.set({ color: "#3aa757" }, function () {
     console.log("The color is green.");
--- plugin-dist/popup/js/index.js
+++ plugin-dist/popup/js/index.js
@@ -2,10 +2,13 @@ let changeColor = document.getElementById("changeColor");
 
 // button 的点击事件
 changeColor.addEventListener("click", () => {
+  console.log('change color div click');
   // 获取tabs
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    console.log('get current tab:', tabs);
     // 获取 storage 本地存储
     chrome.storage.sync.get("color", ({ color }) => {
+      console.log('get storage color:', color);
       // 操作tab
       chrome.tabs.executeScript(tabs[0].id, {
         code: 'document.body.style.backgroundColor = "' + color + '";',
