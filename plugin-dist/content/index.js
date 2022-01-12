2a23b978fa8be76a64ec2d4a3d8c5c0e57f80987
宁振宇 <ningzhenyu@youzan.com>
2022-01-12 17:14:21 +0800

    backgroun-content 通信

--- plugin-dist/content/index.js
+++ plugin-dist/content/index.js
@@ -1,1 +1,18 @@
 console.log('content sctipt run');
+
+// get popup2content info
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log(request.info)
+  sendResponse('我收到了，background~')
+});
+
+// Chrome提供的大部分API是不支持在content_scripts中运行
+// sendMessage onMessage 是可以使用
+// run!
+chrome.runtime.sendMessage({
+  info: "我是 content.js"
+}, res => {
+  // 答复
+  // alert(res)
+  console.log(res);
+})
